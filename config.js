39b0f45b10d942d71fbd0d3a2c77f783997dacb5
xLordkayx <westwalley061@gmail.com>
2025-02-06 20:03:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG,
-ALIVE_MSG: process.env.ALIVE_MSG,
+ALIVE_IMG: process.env.ALIVE_IMG || ""
+ALIVE_MSG: process.env.ALIVE_MSG || "I'm alive Master🤲🙇"
 };
